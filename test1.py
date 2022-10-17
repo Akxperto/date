b7b00a769bf5c32d46997f234eeae3d4f1c71fdf
Akxperto <42714379+Akxperto@users.noreply.github.com>
2022-10-17 17:55:45 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>date</title>
+</head>
+<body>
 <iframe src="https://trinket.io/embed/python3/ff2ecbe490?outputOnly=true&runOption=run" width="100%" height="356" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
